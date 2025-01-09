8f5e7177202bc041f4eddcb010f87d573f40b745
Boris Aizenberg <boris_aizenberg@epam.com>
2025-01-09 12:00:35 +0500

    initial compilable

--- src/main/java/jdbc/CustomDataSource.java
+++ src/main/java/jdbc/CustomDataSource.java
@@ -5,6 +5,13 @@ import javax.sql.DataSource;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.logging.Logger;
+import jdbc.CustomConnector;
+
 @Getter
 @Setter
 public class CustomDataSource implements DataSource {
@@ -13,13 +20,86 @@ public class CustomDataSource implements DataSource {
     private final String url;
     private final String name;
     private final String password;
+    private final CustomConnector connector;
 
     private CustomDataSource(String driver, String url, String password, String name) {
+        this.driver = driver;
+        this.url = url;
+        this.name = name;
+        this.password = password;
+        this.connector = new CustomConnector();
     }
 
     public static CustomDataSource getInstance() {
-
+        if (instance == null) {
+            Properties props = new Properties();
+            try {
+                Class.forName(props.driver);
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+            instance = new CustomDataSource(props.driver, props.url, props.password, props.name);
+        }
         return instance;
     }
 
+    @Override
+    public Connection getConnection() throws SQLException {
+        return connector.getConnection(url);
+    }
+
+    @Override
+    public Connection getConnection(String username, String password) throws SQLException {
+        return connector.getConnection(url, username, password);
+    }
+
+    @Override
+    public PrintWriter getLogWriter() throws SQLException {
+        return null;
+    }
+
+    @Override
+    public void setLogWriter(PrintWriter out) throws SQLException {
+
+    }
+
+
+    @Override
+    public void setLoginTimeout(int seconds) throws SQLException {
+
+    }
+
+    @Override
+    public int getLoginTimeout() throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+        return null;
+    }
+
+    @Override
+    public <T> T unwrap(Class<T> iface) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public boolean isWrapperFor(Class<?> iface) throws SQLException {
+        return false;
+    }
+
+    private static class Properties {
+        private final String driver;
+        private final String url;
+        private final String name;
+        private final String password;
+
+        private Properties() {
+            this.driver = System.getProperty("driver");
+            this.name = System.getProperty("name");
+            this.url = System.getProperty("url");
+            this.password = System.getProperty("password");
+        }
+    }
 }
--- src/main/java/jdbc/SimpleJDBCRepository.java
+++ src/main/java/jdbc/SimpleJDBCRepository.java
@@ -6,9 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -21,26 +20,53 @@ public class SimpleJDBCRepository {
     private PreparedStatement ps = null;
     private Statement st = null;
 
-    private static final String createUserSQL = "";
-    private static final String updateUserSQL = "";
-    private static final String deleteUser = "";
-    private static final String findUserByIdSQL = "";
-    private static final String findUserByNameSQL = "";
-    private static final String findAllUserSQL = "";
+    private static final String createUserSQL = "INSERT INTO public.myusers (\n" +
+            "id, firstname, lastname, age) VALUES (\n" +
+            "'?'::integer, '?'::character varying, '?'::character varying, '?'::integer)\n" +
+            " returning id;";
+    private static final String updateUserSQL = "UPDATE public.myusers SET\n" +
+            "firstname = '?'::character varying WHERE\n" +
+            "id = 1;";
+    private static final String deleteUser = "DELETE FROM public.myusers\n" +
+            "    WHERE id IN\n" +
+            "        (?);";
+    private static final String findUserByIdSQL = "SELECT * FROM public.myusers WHERE id = ?;";
+    private static final String findUserByNameSQL = "SELECT * FROM public.myusers WHERE name = ? LIMIT 1;";
+    private static final String findAllUserSQL = "SELECT * FROM public.myusers;";
 
     public Long createUser() {
+        return null;
     }
 
     public User findUserById(Long userId) {
+        return null;
     }
 
     public User findUserByName(String userName) {
+        return null;
     }
 
     public List<User> findAllUser() {
+        List<User> queryResult = new ArrayList<>();
+        try (Statement st = connection.createStatement()) {
+            ResultSet result = st.executeQuery(findAllUserSQL);
+            while (result.next()) {
+                User user = new User();
+                user.setId(result.getLong("id"));
+                user.setAge(result.getInt("age"));
+                user.setFirstName(result.getString("firstname"));
+                user.setLastName(result.getString("lastname"));
+                queryResult.add(user);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return queryResult;
     }
 
     public User updateUser() {
+        return null;
     }
 
     private void deleteUser(Long userId) {
